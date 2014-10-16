61753f27cba0e7a66c27eec38e92d28a8f844170
jeremy <jeremy@192.168.1.95>
2014-10-16 13:31:32 +0200

    Nouveau test...

--- Pi2test/src/pi2.java
+++ Pi2test/src/pi2.java
@@ -5,6 +5,7 @@ public class pi2 {
 		// TODO Auto-generated constructor stub
 		//Prions pour que ça marche !
 		System.out.print("Ca marche, blaireau !");
+		System.out.print("Je suis pas sûr de moi par contre !");
 	}
 
 }
